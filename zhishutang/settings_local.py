a0d3985cc6ea35bfa6978f4ee036b3c8f9d0f651
guochenpeng <guochenpeng@yun-jinrong.com>
2017-09-20 21:40:27 +0800

    更新.gitignore

--- zhishutang/settings_local.py
+++ zhishutang/settings_local.py
@@ -1,13 +1,9 @@
-SALT_API = {"url": "https://172.16.234.105:8000",
-            "user": "salt",
-            "password": "salt"
-}
+#! /usr/sbin/python2.7
+# __*__ coding: utf-8 __*__
 
-SSH_LOCAL = {"username": "appuser",
-             "password": "4rfv5tgb^YHN"
-}
 
 import string, random
+
 
 def random_char(y):
     return ''.join(random.choice(string.ascii_letters) for x in range(y))
